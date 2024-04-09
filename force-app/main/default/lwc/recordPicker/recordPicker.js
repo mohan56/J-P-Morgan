ebafd56366100f13e6b0d0d7a492e96a915a9589
Abs-mohan <mohanjani502@gmail.com>
2024-04-09 13:24:52 +0530

    Deleted the Comment Line of record picker

--- force-app/main/default/lwc/recordPicker/recordPicker.js
+++ force-app/main/default/lwc/recordPicker/recordPicker.js
@@ -5,7 +5,6 @@ export default class RecordPicker extends LightningElement {
 
     handleValueChange(event) {
         const selectedRecordId = event.detail;
-        // Handle the selected record ID, for example, log it to the console
         console.log('Selected Contact ID: ' + selectedRecordId);
     }
 }
