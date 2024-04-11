b5621c9e77e22ffee99781c78d2563bba76311bc
mohan56 <mohanjani502@gmail.com>
2024-04-11 13:53:32 +0530

    DELETED

--- force-app/main/default/lwc/childCompoent/childCompoent.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement,api } from 'lwc';
-
-export default class ChildCompoent extends LightningElement {
-
-
-    @api receivedDataFromParent=0;
-   @api receivedDatafromParentOnButtonClick=0;
-}
--- force-app/main/default/lwc/popUpMessage/popUpMessage.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class PopUpMessage extends LightningElement {
-
-    closePopupHandler(){
-        const closedPopupEvent= new CustomEvent('closemodel',{detail : false});
-         this.dispatchEvent(closedPopupEvent);
-    }
-}
